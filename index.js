cf665f4e23a8d76f071416dd419314f54a386710
igor.efrem <igor.efrem@carasent.com>
2023-05-11 20:21:37 +0200

    updated packages

--- index.js
+++
@@ -1,8 +0,0 @@
-export * from './src/global';
-export * from './src/interfaces';
-export * from './src/registerASMAMicroApps';
-export * from './src/registry/microapp-registry';
-export * from './src/loader-queue/MfComponentLoader';
-export * from './src/loader-queue/LoaderQueue';
-export * from './src/loadASMAMicroApp';
-//# sourceMappingURL=index.js.map
--- index.ts
+++
@@ -1,7 +0,0 @@
-export * from './src/global'
-export * from './src/interfaces'
-export * from './src/registerASMAMicroApps'
-export * from './src/registry/microapp-registry'
-export * from './src/loader-queue/MfComponentLoader'
-export * from './src/loader-queue/LoaderQueue'
-export * from './src/loadASMAMicroApp'
